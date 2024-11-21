d878c341add1aabd492b123d6470f2f8dd236e44
web223dev <ruslanus866@outlook.com>
2024-11-21 08:35:09 -0800

    add github link on homepage

--- components/Social.jsx
+++ components/Social.jsx
@@ -1,12 +1,12 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-import { FaGithub, FaLinkedinIn, FaYoutube, FaTwitter } from 'react-icons/fa';
+import { FaGithub, FaLinkedinIn, FaYoutube, FaTwitter } from "react-icons/fa";
 
 const socials = [
-  { icon: <FaGithub />, path: '' },
-  { icon: <FaLinkedinIn />, path: '' },
-  { icon: <FaYoutube />, path: '' },
-  { icon: <FaTwitter />, path: '' },
+  { icon: <FaGithub />, path: "https://github.com/web223dev" },
+  { icon: <FaLinkedinIn />, path: "" },
+  { icon: <FaYoutube />, path: "" },
+  { icon: <FaTwitter />, path: "" },
 ];
 
 const Social = ({ containerStyles, iconStyles }) => {
